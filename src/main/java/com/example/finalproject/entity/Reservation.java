153ede3f0fb6874d4589038e4693bb4c06d2e781
Khayal Sharifov <sharifovkhayal6@gmail.com>
2025-07-16 16:02:31 +0400

    Reservation entity əlavə edildi

---
+++ src/main/java/com/example/finalproject/entity/Reservation.java
@@ -0,0 +1,39 @@
+package com.example.finalproject.entity;
+
+import com.example.finalproject.entity.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "reservations")
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    private Double totalAmount;
+
+    @OneToOne(mappedBy = "reservation")
+    private Payment payment;
+}
