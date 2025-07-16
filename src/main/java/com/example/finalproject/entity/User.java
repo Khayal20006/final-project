c79bb5e7f6738378bf02284ddb74d6772da95270
Khayal Sharifov <sharifovkhayal6@gmail.com>
2025-07-16 16:02:30 +0400

    User entity əlavə edildi

---
+++ src/main/java/com/example/finalproject/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.finalproject.entity;
+
+import com.example.finalproject.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "user",cascade =CascadeType.ALL)
+    private List<Reservation>reservations;
+}
