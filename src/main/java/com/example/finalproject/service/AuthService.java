8893a86595b0a81b1bc0bc2b339cc3df16cc156a
Khayal Sharifov <sharifovkhayal6@gmail.com>
2025-07-22 13:10:28 +0400

    Auth ucun ayri service yaradildi service register ve login etmey ucundur

---
+++ src/main/java/com/example/finalproject/service/AuthService.java
@@ -0,0 +1,65 @@
+package com.example.finalproject.service;
+import com.example.finalproject.dto.LoginRequestDto;
+import com.example.finalproject.dto.LoginResponseDto;
+import com.example.finalproject.dto.UserDto;
+import com.example.finalproject.entity.User;
+import com.example.finalproject.entity.enums.Role;
+import com.example.finalproject.repository.UserRepository;
+import com.example.finalproject.util.JwtUtil;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    @Transactional
+    public LoginResponseDto login(LoginRequestDto request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+        String token = jwtUtil.generateToken(user.getEmail(), user.getRole().name());
+
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setFullName(user.getFullName());
+        userDto.setEmail(user.getEmail());
+        userDto.setRole(user.getRole());
+
+        return new LoginResponseDto(token, "Login successful", userDto);
+
+    }
+    @Transactional
+    public LoginResponseDto register(UserDto userDto) {
+        if (userRepository.findByEmail(userDto.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = new User();
+        user.setFullName(userDto.getFullName());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setRole(Role.USER);
+
+        userRepository.save(user);
+
+        String token = jwtUtil.generateToken(user.getEmail(), user.getRole().name());
+
+        userDto.setId(user.getId());
+        userDto.setPassword(null);
+
+        return new LoginResponseDto(token, "Registration successful", userDto);
+    }
+
+}
